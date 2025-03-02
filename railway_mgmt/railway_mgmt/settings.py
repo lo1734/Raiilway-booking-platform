ed6570a64ff481bf12307371ffccc419ad928025
Lohitaksha <112050224+lo1734@users.noreply.github.com>
2025-03-02 23:06:06 +0530

    Update settings.py

--- railway_mgmt/railway_mgmt/settings.py
+++ railway_mgmt/railway_mgmt/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uimq636biu=t()yu&ypqi38hfy=#^s)vs$p^puf9!wh(&un9z2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
